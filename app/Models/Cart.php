03c7217b750205453b322a0b007a34a3b8556485
aftar51 <afftardarda@gmail.com>
2024-04-26 10:51:23 +0700

    cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id'
+    ];
+
+    protected function product()
+        {
+            return $this->belongsTo(Product::class);
+        }
+    
+    protected function user()
+        {
+            return $this->belongsTo(User::class);
+        }
 }
